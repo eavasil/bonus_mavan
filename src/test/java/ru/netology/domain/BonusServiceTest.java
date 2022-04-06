fc7e596bfdd14884c20f5b4476e9b99a382303aa
eavasil <dan_kat@mail.ru>
2022-04-06 21:45:58 +0700

    4 test

--- src/test/java/ru/netology/domain/BonusServiceTest.java
+++ src/test/java/ru/netology/domain/BonusServiceTest.java
@@ -39,19 +39,31 @@ public class BonusServiceTest {
     }
 
     @org.junit.jupiter.api.Test
-    void shouldCalculateForNotRegistered() {
+    void shouldCalculateForNotRegisteredUnderLimit() {
         BonusService service = new BonusService();
-
-        // подготавливаем данные:
         long amount = 1000_60;
         boolean registered = false;
         long expected = 10;
-
-        // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForNotRegisteredMoreLimit() {
+        BonusService service = new BonusService();
+        long amount = 6_000_000;
+        boolean registered = false;
+        long expected = 500;
+        long actual = service.calculate(amount, registered);
         assertEquals(expected, actual);
     }
 
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredMoreLimit() {
+        BonusService service = new BonusService();
+        long amount = 6_000_000;
+        boolean registered = true;
+        long expected = 500;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
 }
