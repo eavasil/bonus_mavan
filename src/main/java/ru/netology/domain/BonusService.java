b6a8b658d0b8c61a90cd8005250e100dd086c07b
eavasil <dan_kat@mail.ru>
2022-04-12 20:53:51 +0700

    4.1 test

--- src/main/java/ru/netology/domain/BonusService.java
+++ src/main/java/ru/netology/domain/BonusService.java
@@ -3,8 +3,8 @@ package ru.netology.domain;
 public class BonusService {
     public long calculate(long amount, boolean registered) {
         int percent = registered ? 3 : 1;
-        long bonus = amount * percent / 100 / 100;
         long limit = 500;
+        long bonus = amount * percent / 100 / 100;
         if (bonus > limit) {
             bonus = limit;
         }
